5b82461672754e7dec378006d2b3d42d814ac08a
Jake Kromer <kromer.62@osu.edu>
2022-03-28 19:27:48 -0400

    Part Four

--- Project4Life.Data/StoreContext.cs
+++ Project4Life.Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Project4Life.Domain.Catalog;
+using Project4Life.Domain.Orders;
 using Microsoft.EntityFrameworkCore;
 
 namespace Project4Life.Data
@@ -10,11 +11,13 @@ namespace Project4Life.Data
         { }
 
         public DbSet<Item> Items { get; set; }
-
+        public DbSet<Order> Orders { get; set; }
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             DbInitializer.Initialize(builder);
         }
     }
+    
 }
