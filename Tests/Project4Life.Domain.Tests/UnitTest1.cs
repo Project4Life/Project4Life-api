87488bde3a64bccdac11eaa17ea6b135ea46cd79
HunterGrim <grim.103@osu.edu>
2022-04-16 12:03:34 -0400

    Page 1 Lab 5

---
+++ Tests/Project4Life.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project4Life.Domain.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
