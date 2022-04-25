47d335db28e7b3d22937bbc5d3eb90309144957c
yashkantak <yashkantak@gmail.com>
2022-04-24 20:10:43 -0400

    adding CORS support

--- Project4Life.Api/Program.cs
+++ Project4Life.Api/Program.cs
@@ -12,6 +12,15 @@ builder.Services.AddDbContext<StoreContext> (options =>
     options.UseSqlite("Data Source=../Registrar.sqlite",
     b => b.MigrationsAssembly("Project4Life.Api"))
 );
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,6 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
